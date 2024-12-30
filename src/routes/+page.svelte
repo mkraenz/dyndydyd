07ffc870b6fe9eaf013c0e3f5494c4b89439e516
Mirco Kraenz <software@kraenz.eu>
2024-12-30 17:16:24 +0100

    refactor(#1): remove dead code FilterButtons, TasksStats

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	import AppTitle from '../stories/AppTitle.svelte';
-	import FilterButtons from '../stories/FilterButtons.svelte';
 	import TaskForm from '../stories/TaskForm.svelte';
 	import TaskList from '../stories/TaskList.svelte';
 	import * as m from '$lib/paraglide/messages';
 	import db from '$lib/db';
 	import type { PageData } from './$types';
 	import { getTasksContext, setTasksContext } from '$lib/state/TasksState.svelte';
-	import TasksStats from '../stories/TasksStats.svelte';
 	import MoreTasksListActions from '../stories/MoreTasksListActions.svelte';
 
 	let { data }: { data: PageData } = $props();
@@ -20,8 +18,6 @@
 	<AppTitle />
 	<TaskForm />
 	{#if tasksState.allTasks.length}
-		<!-- <TasksStats />
-		<FilterButtons /> -->
 		<MoreTasksListActions />
 	{:else}
 		<p>{m.get_started()}</p>
