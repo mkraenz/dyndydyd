d4925f7d06506b0de8902dfb6712239914f337df
Mirco Kraenz <software@kraenz.eu>
2024-12-31 22:56:11 +0100

    feat(#1): offline indicator

--- src/stories/AppTitle.svelte
+++ src/stories/AppTitle.svelte
@@ -5,6 +5,10 @@
 	import { goto } from '$app/navigation';
 	import type { AvailableLanguageTag } from '$lib/paraglide/runtime';
 	import { languageTag, availableLanguageTags } from '$lib/paraglide/runtime.js';
+	import CloudOff from 'virtual:icons/mdi/cloud-off';
+	import { browser } from '$app/environment';
+
+	let online = $state(false);
 
 	function switchToLanguage(newLanguage: AvailableLanguageTag) {
 		const canonicalPath = i18n.route($page.url.pathname);
@@ -23,16 +27,26 @@
 	};
 </script>
 
+<svelte:window bind:online />
+
 <div class="container">
 	<h1>{m.title()}</h1>
-	<!-- <button onclick={() => switchToLanguage('de')}>en</button>
-	<button onclick={() => switchToLanguage('en')}>de</button> -->
-	<button class="outline secondary" onclick={nextLanguage}>{emoji[languageTag()]}</button>
+	<div class="button-container">
+		<!-- don't include icon in SSR since it will otherwise always be rendered and cause the UI to glitch on load in the client -->
+		<CloudOff width="2rem" height="2rem" display={!browser || online ? 'none' : 'block'} />
+		<button class="outline secondary" onclick={nextLanguage}>{emoji[languageTag()]}</button>
+	</div>
 </div>
 
 <style>
 	.container {
 		display: flex;
 		justify-content: space-between;
+	}
+	.button-container {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		justify-content: flex-end;
 	}
 </style>
