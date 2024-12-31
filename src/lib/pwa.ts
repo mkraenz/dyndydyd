2759fd0d250fbdcd111bf5648608aa0e042d288d
Mirco Kraenz <software@kraenz.eu>
2024-12-31 17:33:52 +0100

    feat(#1): autoupdate service worker

--- src/lib/pwa.ts
+++ src/lib/pwa.ts
@@ -1,16 +1,14 @@
-import * as m from '$lib/paraglide/messages';
-
 export async function detectServiceWorkerUpdate() {
 	const registration = await navigator.serviceWorker.getRegistration();
 	registration?.addEventListener('updatefound', () => {
 		const newServiceWorker = registration.installing;
 		newServiceWorker?.addEventListener('statechange', (e) => {
 			if (newServiceWorker.state === 'installed') {
-				// remember: confirm is like alert and blocking the code execution!
-				const confirmed = confirm(m.service_worker_update_available());
-				if (confirmed) {
-					newServiceWorker.postMessage({ type: 'SERVICE_WORKER_SKIP_WAITING' });
-				}
+				// // remember: confirm is like alert and blocking the code execution!
+				// const confirmed = confirm(m.service_worker_update_available());
+				// if (confirmed) {
+				newServiceWorker.postMessage({ type: 'SERVICE_WORKER_SKIP_WAITING' });
+				// }
 			}
 		});
 	});
